e5963f444d6dff7779368168d629ad81c8a628c5
tasneemYjh <to_to_5111@hotmail.com>
2024-05-15 18:46:12 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,32 +1,23 @@
 import streamlit as st
 
-st.title('Job Postings in :green[Saudi Arabia]')
 
 
-st.text('This is some text.')
-st.image('https://example.com/your-image.jpg', caption='Your Image Caption', use_column_width=True)
+st.title('Job Postings in :green[Saudi Arebia]')
+saudi_flag = """:white_medium_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::green_square::
+"""
+st.markdown(f"Saudi Flag: \n\n{saudi_flag}")
+st.text("""In recent years, Saudi Arabia has seen significant growth in job opportunities across various sectors. This analysis aims to provide insights into job postings in Saudi Arabia, highlighting key trends and patterns in the job market.
+""")
 
 
 
-def set_background(color):
-    hex_color = f"#{color}"
-    html_code = f"""
-    <style>
-    body {{
-        background-color: {hex_color};
-    }}
-    </style>
-    """
-    st.markdown(html_code, unsafe_allow_html=True)
 
-# Set background color
-set_background("f0f0f0")
-
-# Display content
-st.title("Job Postings in Saudi Arabia")
+st.image('', caption='Your Image Caption', use_column_width=True)
 
 
-#st.markdown("<h2 style='text-align: center; color: green;'>Job Postings in Saudi Arabia</h2>", unsafe_allow_html=True)
+
+
+st.markdown("<h2 style='text-align: center; color: green;'>Job Postings in Saudi Arabia</h2>", unsafe_allow_html=True)
 
 #st.title('Job Postings in :green[Saudi Arabia]')
 st.text('This is some text.')
