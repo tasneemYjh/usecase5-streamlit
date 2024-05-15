c92c332ffa74fdcf221404c1b88cd2104b53b947
tasneemYjh <to_to_5111@hotmail.com>
2024-05-15 18:56:42 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 
-st.image('')
+st.image('image1.jpeg')
 st.title('Job Postings in :green[Saudi Arebia]')
 
 st.text("""In recent years, Saudi Arabia has seen significant growth in job opportunities across various sectors. This analysis aims to provide insights into job postings in Saudi Arabia, highlighting key trends and patterns in the job market.
