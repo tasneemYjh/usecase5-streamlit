6f97bc4659293c47dbcb7e600db056ac13827aa0
tasneemYjh <to_to_5111@hotmail.com>
2024-05-15 18:00:53 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,17 @@
 import streamlit as st
 
-st.write('Hello world!')
+st.title('Job Postings in :green[Saudi Arabia]')
+
+st.caption('')
+st.text('This is some text.')
+st.image('https://example.com/your-image.jpg', caption='Your Image Caption', use_column_width=True)
+
+
+st.title('Job Postings in :green[Saudi Arabia]')
+st.text('This is some text.')
+st.image('https://example.com/your-image.jpg', caption='Your Image Caption', use_column_width=True)
+
+
+st.title('Job Postings in :green[Saudi Arabia]')
+st.text('This is some text.')
+st.image('https://example.com/your-image.jpg', caption='Your Image Caption', use_column_width=True)
