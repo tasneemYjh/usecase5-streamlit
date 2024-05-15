f8e3b8ecdb963330c0b64382ca30edd4e84217e7
tasneemYjh <to_to_5111@hotmail.com>
2024-05-15 18:22:34 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,8 +8,25 @@ st.image('https://example.com/your-image.jpg', caption='Your Image Caption', use
 
 
 
+def set_background(color):
+    hex_color = f"#{color}"
+    html_code = f"""
+    <style>
+    body {{
+        background-color: {hex_color};
+    }}
+    </style>
+    """
+    st.markdown(html_code, unsafe_allow_html=True)
 
-st.markdown("<h2 style='text-align: center; color: green;'>Job Postings in Saudi Arabia</h2>", unsafe_allow_html=True)
+# Set background color
+set_background("f0f0f0")
+
+# Display content
+st.title("Job Postings in Saudi Arabia")
+
+
+#st.markdown("<h2 style='text-align: center; color: green;'>Job Postings in Saudi Arabia</h2>", unsafe_allow_html=True)
 
 #st.title('Job Postings in :green[Saudi Arabia]')
 st.text('This is some text.')
