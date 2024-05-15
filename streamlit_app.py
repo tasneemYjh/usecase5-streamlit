6032476974b741419d16f51ac143964f4fa85452
tasneemYjh <to_to_5111@hotmail.com>
2024-05-15 18:59:55 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,13 @@ import streamlit as st
 
 
 st.image('image1.jpeg')
-st.title('Job Postings in :green[Saudi Arebia]')
+st.title('Job Postings in :green[Saudi Arabia]')
 
-st.text("""In recent years, Saudi Arabia has seen significant growth in job opportunities across various sectors. This analysis aims to provide insights into job postings in Saudi Arabia, highlighting key trends and patterns in the job market.
+st.markdown("""
+In recent years, Saudi Arabia has seen significant growth in job opportunities across various sectors.
+This analysis aims to provide insights into job postings in Saudi Arabia, highlighting key trends and patterns in the job market.
 """)
+
 
 
 
